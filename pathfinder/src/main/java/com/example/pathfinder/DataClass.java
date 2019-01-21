41a53d2f8b423e42c940cff4f069566e62d3b0e6
BijinRaveendran94 <bijin@vaspp.com>
2019-01-21 11:13:17 +0530

    final commit for testing

--- pathfinder/src/main/java/com/example/pathfinder/DataClass.java
+++ pathfinder/src/main/java/com/example/pathfinder/DataClass.java
@@ -9,10 +9,22 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
 public class DataClass  {
+
+    private JSONArray nodes;
+
+    ArrayList<JSONObject> nodesHash;
+    List<JSONObject> waypoints;
+    LatLng sourceNode;
+    LatLng destinationNode;
+    ArrayList<LatLng> visitedNodes;
+    JSONObject shortestNodesHash;
+    ArrayList<ArrayList<LatLng>> shortestWaypoints = new ArrayList<ArrayList<LatLng>>();
+
 
     public static List<JSONObject> getWaypointsForRoom(String json, JSONObject room) {
         List<JSONObject> arrayList = new ArrayList<JSONObject>();
@@ -126,7 +138,7 @@ public class DataClass  {
 
         return new Float(distance * meterConversion).doubleValue();
     }
-    
+
     public static ArrayList<JSONObject> getNodesForWaypoints(List<JSONObject> Waypoints) {
         ArrayList<JSONObject> node = new ArrayList<JSONObject>();
         try {
@@ -330,5 +342,254 @@ public class DataClass  {
 
         }
         return node;
+    }
+
+    public JSONObject getPathFromSource(LatLng location, LatLng destination, List<JSONObject> Waypoints) {
+        try {
+
+            nodesHash = getNodesForWaypoints(Waypoints);
+            visitedNodes = new ArrayList<LatLng>();
+            shortestNodesHash = new JSONObject();
+            waypoints = Waypoints;
+            sourceNode = getNearestNodeFor(location);
+            destinationNode = getNearestNodeFor(destination);
+            double distance = 0;
+
+            JSONObject nodes = getPathFromSource(sourceNode, distance);
+
+            return nodes;
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+        return null;
+    }
+
+    private LatLng getNearestNodeFor(LatLng location) {
+        try {
+
+            LatLng selectedNode = null;
+
+            double distance = 0;
+
+            for (int v = 0; v < nodesHash.size(); v++) {
+                JSONObject node = nodesHash.get(v);
+                Iterator<String> keys = node.keys();
+                while (keys.hasNext()) {
+                    String key = keys.next();
+                    String Pointb = key.replace("{", " ");
+                    String PointBb = Pointb.replace("}", " ");
+                    String[] latlongb = PointBb.split(",");
+                    double latitude = Double.parseDouble(latlongb[0]);
+                    double longitude = Double.parseDouble(latlongb[1]);
+                    LatLng LatLng = new LatLng(latitude, longitude);
+
+                    double dist = distance(location.latitude, location.longitude, LatLng.latitude, LatLng.longitude);
+
+                    if (dist < distance || distance == 0) {
+                        distance = dist;
+                        selectedNode = LatLng;
+                    }
+                }
+            }
+
+            return selectedNode;
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+        return null;
+    }
+
+    public JSONObject getPathFromSource(LatLng source, double distance) {
+
+        try {
+
+            if (source.toString().equals(destinationNode.toString())) {
+                JSONArray jsonArray = new JSONArray();
+
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("Distance", distance);
+                jsonArray.put(source.toString());
+                jsonObject.put("Nodes", (Object) jsonArray);
+                return jsonObject;
+            }
+
+            if (visitedNodes.contains(source)) {
+
+                JSONObject jsonObject = new JSONObject();
+                String sourceName = source.toString();
+                if (shortestNodesHash.length() > 0) {
+                    Iterator<String> keys = shortestNodesHash.keys();
+                    while (keys.hasNext()) {
+                        String key = keys.next();
+                        if (key.equalsIgnoreCase(sourceName)) {
+                            jsonObject = shortestNodesHash.getJSONObject(sourceName);
+                            return jsonObject;
+                        }
+                    }
+                } else {
+                    return null;
+                }
+                return null;
+            }
+
+            visitedNodes.add(source);
+            ArrayList<JSONObject> connectingNodes = new ArrayList<JSONObject>();
+
+            List<Double> integerList = new ArrayList<Double>();
+            List<JSONObject> items = new ArrayList<JSONObject>();
+            JSONArray jsonArray = new JSONArray();
+
+            for (int v = 0; v < nodesHash.size(); v++) {
+                JSONObject node = nodesHash.get(v);
+                Iterator<String> keys = node.keys();
+                while (keys.hasNext()) {
+                    String key = keys.next();
+                    String Pointb = key.replace("{", " ");
+                    String PointBb = Pointb.replace("}", " ");
+                    String[] latlongb = PointBb.split(",");
+                    double latitude = Double.parseDouble(latlongb[0]);
+                    double longitude = Double.parseDouble(latlongb[1]);
+
+                    LatLng LatLng = new LatLng(latitude, longitude);
+
+                    if (LatLng.equals(source)) {
+
+                        jsonArray = nodesHash.get(v).getJSONArray(key);
+
+                        for (int i = 0; i < jsonArray.length(); i++) {
+
+                            JSONObject jsonObject = jsonArray.getJSONObject(i);
+                            double Distance = (double) jsonObject.get("Distance");
+                            integerList.add(Distance);
+                            String PointB = jsonObject.getString("PointB");
+                            String PointA = jsonObject.getString("PointA");
+                            JSONObject jsondata = new JSONObject();
+                            jsondata.put("Distance", Distance);
+                            jsondata.put("PointA", PointA);
+                            jsondata.put("PointB", PointB);
+                            items.add(jsondata);
+                        }
+                        Collections.sort(integerList);
+
+                        for (int j = 0; j < integerList.size(); j++) {
+                            double dist = integerList.get(j);
+
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                                double Distance = (double) jsonObject.get("Distance");
+                                if (dist == Distance) {
+                                    for (int k = 0; k < items.size(); k++) {
+                                        double distanceA = (double) items.get(k).get("Distance");
+                                        String PointA = jsonArray.getJSONObject(i).getString("PointA");
+                                        String PointB = jsonArray.getJSONObject(i).getString("PointB");
+
+                                        String pointa = items.get(k).getString("PointA");
+                                        String pointb = items.get(k).getString("PointB");
+                                        if (distanceA == dist && pointa.equalsIgnoreCase(PointA) && pointb.equalsIgnoreCase(PointB)) {
+                                            connectingNodes.add(jsonObject);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            JSONArray nodes = null;
+            double minDistance = 0;
+
+            for (JSONObject edge : connectingNodes) {
+                JSONObject connectingNode = edge;
+                String PointA = connectingNode.getString("PointA");
+                String Pointa = PointA.replace("{", " ");
+                String PointAa = Pointa.replace("}", " ");
+                String[] latlonga = PointAa.split(",");
+                double latitudea = Double.parseDouble(latlonga[0]);
+                double longitudea = Double.parseDouble(latlonga[1]);
+
+                String PointB = connectingNode.getString("PointB");
+                String Pointb = PointB.replace("{", " ");
+                String PointBb = Pointb.replace("}", " ");
+                String[] latlongb = PointBb.split(",");
+                double latitudeb = Double.parseDouble(latlongb[0]);
+                double longitudeb = Double.parseDouble(latlongb[1]);
+
+                LatLng PointaLatLng = new LatLng(latitudea, longitudea);
+                LatLng PointbLatLng = new LatLng(latitudeb, longitudeb);
+
+                String Distance = connectingNode.getString("Distance");
+                double distanceNde = Double.parseDouble(Distance);
+                JSONObject nodesDict = null;
+
+                if (PointaLatLng.equals(source)) {
+                    nodesDict = getPathFromSource(PointbLatLng, distanceNde);
+
+                } else {
+                    nodesDict = getPathFromSource(PointaLatLng, distanceNde);
+                }
+
+                if (nodesDict != null) {
+
+                    double newDistance = (double) nodesDict.get("Distance") + distance;
+
+                    if (newDistance < minDistance || minDistance == 0) {
+                        minDistance = newDistance;
+                        nodes = new JSONArray();
+                        JSONArray jsonArray1 = null;
+                        jsonArray1 = (JSONArray) nodesDict.get("Nodes");
+                        nodes = jsonArray1;
+                    }
+                }
+            }
+
+            if (nodes != null) {
+                if (nodes.length() > 0) {
+                    ArrayList<LatLng> newNodes = new ArrayList<LatLng>();
+                    newNodes.add(source);
+
+                    for (int j = 0; j < nodes.length(); j++) {
+
+                        Object latLng = nodes.get(j);
+                        String Point = latLng.toString();
+
+
+                        String Pointa = Point.replace("lat/lng:", " ");
+                        String PointAa = Pointa.replace("(", " ");
+                        String Pointb = PointAa.replace(")", " ");
+                        String[] latlonga = Pointb.split(",");
+                        double latitudea = Double.parseDouble(latlonga[0]);
+                        double longitudea = Double.parseDouble(latlonga[1]);
+                        LatLng latlg = new LatLng(latitudea, longitudea);
+                        newNodes.add(latlg);
+
+                    }
+
+                    shortestWaypoints.add(newNodes);
+                    JSONObject nodesDictionary = new JSONObject();
+
+                    nodesDictionary.put("Distance", minDistance);
+                    JSONArray arrays = new JSONArray();
+                    for (LatLng node : newNodes) {
+                        arrays.put(node);
+                    }
+                    nodesDictionary.put("Nodes", (Object) arrays);
+                    shortestNodesHash.put(source.toString(), nodesDictionary);
+                    return nodesDictionary;
+                }
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+
+        } catch (ClassCastException e) {
+            e.printStackTrace();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
